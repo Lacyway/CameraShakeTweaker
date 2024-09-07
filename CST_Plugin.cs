cfdccc905b41a8699e04518589e6794683791b11
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-09-07 02:02:23 +0200

    Add project files.

---
+++ CST_Plugin.cs
@@ -0,0 +1,27 @@
+﻿using BepInEx;
+using BepInEx.Configuration;
+using BepInEx.Logging;
+
+namespace CameraShakeTweaker
+{
+	[BepInPlugin("com.lacyway.cst", "CameraShakeTweaker", "1.0.0")]
+	public class CST_Plugin : BaseUnityPlugin
+	{
+		public static ManualLogSource CST_Logger;
+		public static ConfigEntry<float> ShakeAmount;
+
+		protected void Awake()
+		{
+			CST_Logger = Logger;
+			CST_Logger.LogInfo($"{nameof(CST_Plugin)} has been loaded.");
+
+			ShakeAmount = Config.Bind("Config", "Shake Amount", 1f, new ConfigDescription("The percentage to multiply the shake amount with",
+			new AcceptableValueRange<float>(0f, 1f), new ConfigurationManagerAttributes()
+			{
+				ShowRangeAsPercent = true
+			}));
+
+			new ForceEffector_AddForce_Patch().Enable();
+		}
+	}
+}
---
+++ ForceEffector_AddForce_Patch.cs
@@ -0,0 +1,20 @@
+﻿using SPT.Reflection.Patching;
+using System.Linq;
+using System.Reflection;
+
+namespace CameraShakeTweaker
+{
+	public class ForceEffector_AddForce_Patch : ModulePatch
+	{
+		protected override MethodBase GetTargetMethod()
+		{
+			return typeof(ForceEffector).GetMethods().Where(x => x.Name == "AddForce" && x.GetParameters().Length == 4).FirstOrDefault();
+		}
+
+		[PatchPrefix]
+		public static void Prefix(ref float strength)
+		{
+			strength *= CST_Plugin.ShakeAmount.Value;
+		}
+	}
+}
