aa150999ba8b2a894588d6408acd98a9b76d9c85
Lacyway <20912169+Lacyway@users.noreply.github.com>
2024-11-23 22:44:26 +0100

    Update ref and version

--- CST_Plugin.cs
+++ CST_Plugin.cs
@@ -4,7 +4,7 @@ using BepInEx.Logging;
 
 namespace CameraShakeTweaker
 {
-	[BepInPlugin("com.lacyway.cst", "CameraShakeTweaker", "1.0.0")]
+	[BepInPlugin("com.lacyway.cst", "CameraShakeTweaker", "1.1.0")]
 	public class CST_Plugin : BaseUnityPlugin
 	{
 		public static ManualLogSource CST_Logger;
